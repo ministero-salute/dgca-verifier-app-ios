558ba09b8220b9bcbdedc17c440a6114b3c01ac2
eapuzzo <emilio.apuzzo.ea@outlook.it>
2021-10-28 17:12:34 +0200

    feature: pass revoke test
    - Add Delta mockup

--- DGCAVerifier/Services/GatewayConnection/GatewayConnection+Revocation.swift
+++ DGCAVerifier/Services/GatewayConnection/GatewayConnection+Revocation.swift
@@ -31,7 +31,7 @@ extension GatewayConnection {
         let version = progress?.currentVersion
         let chunk = progress?.currentChunk
         
-        getCRL(version: version, chunk: chunk) { crl in
+        getCRLDeltaMOCK(version: version, chunk: chunk) { crl in
             
             guard let crl = crl else {
                 completion?(nil, "server.error.generic.error".localized)
@@ -44,6 +44,40 @@ extension GatewayConnection {
 
     private func getCRL(version: Int?, chunk: Int?, completion: ((CRL?) -> Void)?) {
         let restStartTime = Log.start(key: "[CRL] [REST]")
+        session.request(revocationUrl).response {
+            Log.end(key: "[CRL] [REST]", startTime: restStartTime)
+            
+            let jsonStartTime = Log.start(key: "[CRL] [JSON]")
+            let decoder = JSONDecoder()
+            var data = try? decoder.decode(CRL.self, from: $0.data ?? .init())
+            data?.responseSize = $0.data?.count.doubleValue
+            Log.end(key: "[CRL] [JSON]", startTime: jsonStartTime)
+            
+            guard let crl = data else {
+                completion?(nil)
+                return
+            }
+            completion?(crl)
+        }
+    }
+    
+    private func getCRLDeltaMOCK(version: Int?, chunk: Int?, completion: ((CRL?) -> Void)?) {
+        let restStartTime = Log.start(key: "[CRL] [REST]")
+        
+        var revocationUrl = revocationUrl
+        //MOCK
+        if version == 1 {
+            if chunk == 1 {
+                revocationUrl = "https://run.mocky.io/v3/7d71ba28-6ee0-4e5a-a815-93f3a33e7ec4"
+            }
+            else if chunk == 2 {
+                revocationUrl = "https://run.mocky.io/v3/bf32eb99-1993-4272-b9cc-6522a04be009"
+            }
+            else if chunk == 3{
+                revocationUrl = "https://run.mocky.io/v3/ec6c4335-1e2c-4917-993c-99ea992c0f4e"
+            }
+        }
+        
         session.request(revocationUrl).response {
             Log.end(key: "[CRL] [REST]", startTime: restStartTime)
             
@@ -75,13 +109,26 @@ extension GatewayConnection {
                 completion?(nil)
                 return
             }
-            completion?(self.getCRLStatusMock())
+            
+            //MOCK
+            if version == 0{
+                completion?(self.getCRLStatusMock())
+            }
+            else if version == 1 {
+                completion?(self.getCRLStatusDeltaMock())
+            }
+            else {
+                completion?(status)
+            }
         }
     }
 
     private func getCRLMock(from crl: CRL) -> CRL {
         var newCrl = crl
-        let mockStatus = getCRLStatusMock()
+        var mockStatus = getCRLStatusMock()
+        if crl.version == 2 {
+            mockStatus = getCRLStatusDeltaMock()
+        }
         newCrl.version = mockStatus.version
         newCrl.chunk = mockStatus.chunk
         newCrl.lastChunk = mockStatus.totalChunk
@@ -92,13 +139,25 @@ extension GatewayConnection {
     
     private func getCRLStatusMock() -> CRLStatus {
         var status = CRLStatus()
-        status.fromVersion = 47
-        status.version = 58
+        status.fromVersion = 0
+        status.version = 1
         status.chunk = 1
         status.totalSizeInByte = 8388600
         status.sizeSingleChunkInByte = 838860
-        status.totalChunk = 10
+        status.totalChunk = 1
         status.totalNumberUCVI = 10000
+        return status
+    }
+    
+    private func getCRLStatusDeltaMock() -> CRLStatus {
+        var status = CRLStatus()
+        status.fromVersion = 1
+        status.version = 2
+        status.chunk = 1
+        status.totalSizeInByte = 8388600
+        status.sizeSingleChunkInByte = 838860
+        status.totalChunk = 3
+        status.totalNumberUCVI = 10015
         return status
     }
     
