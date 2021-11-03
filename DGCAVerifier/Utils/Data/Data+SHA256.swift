db047fa31d9f4888bcf94c89182dc2682d5a5e75
eapuzzo <emilio.apuzzo.ea@outlook.it>
2021-11-03 18:36:39 +0100

    feature: pass revoke test
    - Fix sha256 func in Data+SHA256

--- DGCAVerifier/Utils/Data/Data+SHA256.swift
+++ DGCAVerifier/Utils/Data/Data+SHA256.swift
@@ -10,7 +10,7 @@ import CommonCrypto
 
 extension Data{
     public func sha256() -> String{
-        return hexStringFromData(input: digest(input: self as NSData))
+        return base64StringFromData(input: digest(input: self as NSData))
     }
     
     private func digest(input : NSData) -> NSData {
@@ -20,15 +20,7 @@ extension Data{
         return NSData(bytes: hash, length: digestLength)
     }
     
-    private  func hexStringFromData(input: NSData) -> String {
-        var bytes = [UInt8](repeating: 0, count: input.length)
-        input.getBytes(&bytes, length: input.length)
-        
-        var hexString = ""
-        for byte in bytes {
-            hexString += String(format:"%02x", UInt8(byte))
-        }
-        
-        return hexString
+    private  func base64StringFromData(input: NSData) -> String {
+       return input.base64EncodedString()
     }
 }
