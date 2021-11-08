b046068d1a76e939357bd9c317d72d1ffe7e5c0b
eapuzzo <emilio.apuzzo.ea@outlook.it>
2021-11-08 10:37:04 +0100

    feature: pass revoke test
    - Add check CRL Download

--- DGCAVerifier/Pages/Camera/CameraViewController.swift
+++ DGCAVerifier/Pages/Camera/CameraViewController.swift
@@ -98,6 +98,12 @@ class CameraViewController: UIViewController {
         guard !(vc is VerificationViewController) else { return }
         stopRunning()
         hapticFeedback()
+        let isCRLDownloadCompleted = CRLDataStorage.shared.isCRLDownloadCompleted
+        let isCRLAllowed = SettingDataStorage.sharedInstance.getFirstSetting(withName: "DRL_SYNC_ACTIVE")?.boolValue ?? true
+        if !isCRLDownloadCompleted && isCRLAllowed {
+            showAlert(key: "no.crl.download")
+            return
+        }
         coordinator?.validate(payload: payload, country: country, delegate: self)
     }
     
@@ -167,6 +173,19 @@ class CameraViewController: UIViewController {
         DispatchQueue.main.async {
             UINotificationFeedbackGenerator().notificationOccurred(.success)
         }
+    }
+    
+    private func showAlert(key: String) {
+        let alertController = UIAlertController(
+            title: "alert.\(key).title".localized,
+            message: "alert.\(key).message".localized,
+            preferredStyle: .alert
+        )
+        let alertAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
+            self?.coordinator?.dismiss()
+        }
+        alertController.addAction(alertAction)
+        self.present(alertController, animated: true, completion: nil)
     }
 }
 
