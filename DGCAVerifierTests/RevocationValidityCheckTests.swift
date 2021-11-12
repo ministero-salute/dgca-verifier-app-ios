c2cb6de35d198c289929ec212024763238caf33e
fehniix <29045904+Fehniix@users.noreply.github.com>
2021-11-12 11:18:24 +0100

    feature: error-40x-status-test
    - Improved setUpWithError
    - Fixed realm tearDown
    - Improved nonRevokedUVCI test by adding a dummy entry on the CRLDataStorage

--- DGCAVerifierTests/RevocationValidityCheckTests.swift
+++ DGCAVerifierTests/RevocationValidityCheckTests.swift
@@ -34,14 +34,13 @@ class RevocationValidityCheckTests: XCTestCase {
     var payload: String!
     var bodyString: String!
     var realm: Realm!
-
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         payload = "HC1:6BFOXN%TS3DHPVO13J /G-/2YRVA.Q/R8WRU2FCAH9BDF%188WA.*RXU7IJ6W*PP+PDPIGOK-*GN*Q:XJR-GM%O-RQOTAF/8X*G3M9FQH+4J/-K$+CY73JC3MD3IFTKJ3SZ4P:45/GZW4:.AY731MF7FN6LBHKBCAJPF71M3.FJZIJ09B*KNQ579PJMD3+476J3:NB3N5XW49+20CMAHLW 70SO:GOLIROGOAQ5ZU4RKCSHGX2M5C9HHB%LGJZII7JSTNCA7G6MXYQYYQQKRM64YVQB95326FW4AJOMKMV35U:7-Z7QT499RLHPQ15O+4/Z6E 6U963X7$8Q$HMCP63HU$*GT*Q3-Q4+O7F6E%CN4D74DWZJ$7K+ CZEDB2M$9C1QD7+2*KUQFCOYA73A-MG*VM%UUY$MW5LM+GW*1.Q5$Y7M-FSYLJF3*TRJY9R.8VBQA65%UVLXFTYVN T$WM3 UJ16F:S0CLRVJAD7KOB1GV+20RT8S0"
         hcert = HCert(from: payload)
         bodyString = "{\"1\": \"Ministero della Salute\", \"4\": 1623317503, \"6\": 1620925550, \"-260\": {\"1\": {\"r\": [{\"du\": \"2021-10-31\", \"ci\": \"01ITF59E8461C316424486E4A96E10CC039C#6\", \"tg\": \"840539006\", \"is\": \"Ministero della Salute\", \"df\": \"2021-05-04\", \"fr\": \"2021-05-02\", \"co\": \"IT\"}], \"dob\": \"1977-06-16\", \"nam\": {\"gnt\": \"MARILU<TERESA\", \"fnt\": \"DI<CAPRIO\", \"fn\": \"Di Caprio\", \"gn\": \"Marilù Teresa\"}, \"ver\": \"1.0.0\"}}}"
-        
-        realm = try! Realm()
+        if let _ = realm {} else { realm = try! Realm() }
     }
 
     override func tearDownWithError() throws {
@@ -49,7 +48,10 @@ class RevocationValidityCheckTests: XCTestCase {
         payload = nil
         hcert = nil
         bodyString = nil
-        realm = nil
+        
+        try! realm.write() {
+            realm.deleteAll()
+        }
         
         CRLDataStorage.removeAll(hashes: nil)
     }
@@ -62,6 +64,9 @@ class RevocationValidityCheckTests: XCTestCase {
     }
     
     func testNonRevokedUVCI() {
+        let hash: String = "abcdefg".sha256()
+        CRLDataStorage.add(hash: hash, on: realm)
+        
         XCTAssertFalse(RulesValidator.isRevoked(hcert))
     }
     
