840c7aaf34b50e3d7ccb50e9c06d955ed094bed2
Fehniix <29045904+Fehniix@users.noreply.github.com>
2022-01-21 16:35:49 +0100

    feature: code refactor rxswift
    - Updated to last local untracked version

--- DGCAVerifier/Services/GatewayConnection/GatewayConnection.swift
+++ DGCAVerifier/Services/GatewayConnection/GatewayConnection.swift
@@ -18,64 +18,113 @@
  * limitations under the License.
  * ---license-end
  */
-//  
+//
 //  GatewayConnection.swift
 //  DGCAVerifier
-//  
+//
 //  Created by Yannick Spreen on 4/24/21.
-//  
+//
 
 import Foundation
 import Alamofire
 import SwiftDGC
 import SwiftyJSON
+import RxSwift
 
 extension Bundle {
-    func infoForKey(_ key: String) -> String? {
-        return (Bundle.main.infoDictionary?[key] as? String)?.replacingOccurrences(of: "\\", with: "")
-    }
+	func infoForKey(_ key: String) -> String? {
+		return (Bundle.main.infoDictionary?[key] as? String)?.replacingOccurrences(of: "\\", with: "")
+	}
 }
 
 class GatewayConnection {
-    
-    let baseUrl: String
-    let session: Session
-    var timer: Timer?
-    
-    private let certificateFilename: String
-    private let certificateEvaluator: String
-    
-    static let shared = GatewayConnection()
-    
-    private init() {
-        baseUrl = Bundle.main.infoForKey("baseUrl")!
-        certificateFilename = Bundle.main.infoForKey("certificateFilename")!
-        certificateEvaluator = Bundle.main.infoForKey("certificateEvaluator")!
-        
-        // Init certificate for pinning
-        let filePath = Bundle.main.path(forResource: certificateFilename, ofType: nil)!
-        let data = try! Data(contentsOf: URL(fileURLWithPath: filePath))
-        let certificate = SecCertificateCreateWithData(nil, data as CFData)!
-                    
-        // Init session
-        let evaluators = [certificateEvaluator: PinnedCertificatesTrustEvaluator(certificates: [certificate])]
-        session = Session(serverTrustManager: ServerTrustManager(evaluators: evaluators))
-    }
-    
-    func initialize(completion: (()->())? = nil) {
-        timer?.invalidate()
-        timer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { _ in
-            self.trigger(completion: completion)
-        }
-        timer?.tolerance = 5.0
-        self.trigger(completion: completion)
-    }
-    
-    func trigger(completion: (()->())? = nil) {
-        guard LocalData.sharedInstance.lastFetch.timeIntervalSinceNow < -24 * 60 * 60 else {
-            return
-        }
-        completion?()
-    }
-
+	
+	enum GCError: Error {
+		case networkError(AFError)
+		case httpError(Int)
+		case noResponseError
+		case decodeResponseError
+	}
+	
+	
+	let baseUrl: String
+	let session: Session
+	var timer: Timer?
+	
+	private let certificateFilename: String
+	private let certificateEvaluator: String
+	
+	static let shared = GatewayConnection()
+	
+	private init() {
+		baseUrl = Bundle.main.infoForKey("baseUrl")!
+		certificateFilename = Bundle.main.infoForKey("certificateFilename")!
+		certificateEvaluator = Bundle.main.infoForKey("certificateEvaluator")!
+		
+		// Init certificate for pinning
+		let filePath = Bundle.main.path(forResource: certificateFilename, ofType: nil)!
+		let data = try! Data(contentsOf: URL(fileURLWithPath: filePath))
+		let certificate = SecCertificateCreateWithData(nil, data as CFData)!
+		
+		// Init session
+		let evaluators = [certificateEvaluator: PinnedCertificatesTrustEvaluator(certificates: [certificate])]
+		session = Session(serverTrustManager: ServerTrustManager(evaluators: evaluators))
+	}
+	
+	func initialize(completion: (()->())? = nil) {
+		timer?.invalidate()
+		timer = Timer.scheduledTimer(withTimeInterval: 60.0, repeats: true) { _ in
+			self.trigger(completion: completion)
+		}
+		timer?.tolerance = 5.0
+		self.trigger(completion: completion)
+	}
+	
+	func trigger(completion: (()->())? = nil) {
+		guard LocalData.sharedInstance.lastFetch.timeIntervalSinceNow < -24 * 60 * 60 else {
+			return
+		}
+		completion?()
+	}
+	
+	public func get<T: Codable>(url: String) -> RxSwift.Observable<T> {
+		
+		return RxSwift.Observable<T>.create{ observer in
+			
+			let request = self.session.request(url).response {
+				
+				guard $0.error == nil else {
+					observer.on(.error(GCError.networkError($0.error!)))
+					return
+				}
+				
+				guard let response = $0.response else {
+					observer.on(.error(GCError.noResponseError))
+					return
+				}
+				
+				guard response.statusCode == 200 else {
+					//error case
+					observer.on(.error(GCError.httpError(response.statusCode)))
+					return
+				}
+				
+				
+				let decoder = JSONDecoder()
+				let data = try? decoder.decode(T.self, from: $0.data ?? .init())
+				
+				guard let status = data else {
+					observer.on(.error(GCError.decodeResponseError))
+					return
+				}
+				
+				observer.on(.next(status))
+				observer.on(.completed)
+			}
+			return Disposables.create { request.cancel() }
+		}
+		
+	}
+	
+	
 }
